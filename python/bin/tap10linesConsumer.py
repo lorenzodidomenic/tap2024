18429e04f6ed7fed45d9a5ffeaed7dbdc0710afd
Lorenzo <lorenzodidome03@gmail.com>
2024-05-02 20:02:03 +0200

    fix: Kafka connect

--- python/bin/tap10linesConsumer.py
+++ python/bin/tap10linesConsumer.py
@@ -3,10 +3,14 @@ import os
 from kafka import KafkaConsumer
 from json import loads
 
+#dal container ci prendiamo  le varibaili d'ambienete che contengono 
+# #il nome del topic , il group id e l offset
 topic = os.getenv("KAFKA_TOPIC", "tap")
 group_id = os.getenv("GROUP_ID", "my-group")
 offset = os.getenv("OFFSET", "latest")
 print("Paramters are Topic:%s Group id: %s OFFSET: %s " % (topic, group_id, offset))
+
+#startiamo un consumer passandogli topic, kagka server, id del gruppo, e come decodificare il messaggio
 consumer = KafkaConsumer(
      topic,
      bootstrap_servers=['kafkaServer:9092'],
@@ -15,6 +19,7 @@ consumer = KafkaConsumer(
      group_id=group_id,
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
+#per ogni messaggio lo stampiamo
 for message in consumer:
     message = message.value
     print('{} read'.format(message))
--- python/bin/tap10linesProducer.py
+++ python/bin/tap10linesProducer.py
@@ -4,13 +4,17 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
+#ci prendiamo il topic dalle variabili d'ambiente 
 topic = os.getenv("KAFKA_TOPIC", "tap")
 pause = int(os.getenv("pause", 5))
+
+#settiamo per il producer il server e come codificare
 producer = KafkaProducer(bootstrap_servers=['kafkaServer:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 
 e=0
+#in maniera innfinita e ogni 5 secondi mandiamo un numeros
 while(True):
     data = {'number' : e}
     producer.send(topic, value=data)
